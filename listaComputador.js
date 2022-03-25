ed0ed1b68b69e6737e5913c49da2aa815887ee09
GabrielLima29 <101425770+GabrielLima29@users.noreply.github.com>
2022-03-25 15:47:41 -0300

    Update listaComputador.js

--- listaComputador.js
+++ listaComputador.js
@@ -1,31 +1,31 @@
-deixe  dados ;
+let dados;
 
-função  carregarDados ( cb ) {
-    buscar ( "http://3tdsn2022.atwebpages.com/ListarComputor.php" )
-        . then ( conteudo  =>  conteudo . text ( ) )
-        . então ( ( texto )  =>  {
-            dados  =  JSON . analisar ( texto ) ;
-            cb ( ) ;
-        } ) ;
+function carregarDados(cb){
+    fetch("http://3tdsn2022.atwebpages.com/ListarComputador.php")
+        .then(conteudo => conteudo.text())
+        .then((texto) => {
+            dados = JSON.parse(texto);
+            cb();
+        });
 }
 
-função  exibirTabela ( ) {
-    consola . log ( dados ) ;
-    deixe  tabela  =  '' ;
-    for ( deixe a  linha  nos  dados ) {
-        tabela  +=  '<tr>' ;
-        tabela  +=  `<td> ${ dados [ linha ] . PROCESSADOR } </td>` ;
-        tabela  +=  `<td> ${ dados [ linha ] . usb } </td>` ;
-        tabela  +=  `<td> ${ dados [ linha ] . atualizado  ===  '1' ? "Sim" : "Não" } </td>` ;
-        tabela  +=  `<td> ${ dados [ linha ] . dataAtualizacao } </td>` ;
-        tabela  +=  '</tr>' ;
+function exibirTabela(){
+    console.log(dados);
+    let tabela = '';
+    for(let linha in dados){
+        tabela += '<tr>';
+        tabela += `<td>${dados[linha].PrOcEsSaDoR}</td>`;
+        tabela += `<td>${dados[linha].usb}</td>`;
+        tabela += `<td>${dados[linha].atualizado === '1' ? "Sim" : "Não"}</td>`;
+        tabela += `<td>${dados[linha].dataAtualizacao}</td>`;
+        tabela += '</tr>';
     }
-    documento . getElementsByTagName ( 'tbody' ) [ 0 ] . innerHTML  =  tabela ;
+    document.getElementsByTagName('tbody')[0].innerHTML = tabela;
     
 }
 
-função  iniciar ( ) {
-    carregarDados ( displayTabela )
+function iniciar(){
+    carregarDados(exibirTabela)
 }
 
-janela . onload  =  iniciar ;
+window.onload = iniciar;
