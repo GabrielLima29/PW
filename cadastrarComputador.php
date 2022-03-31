e977dbdf2947df3251aa4fec64caa9d4b11fac7e
GabrielLima29 <101425770+GabrielLima29@users.noreply.github.com>
2022-03-31 18:10:25 -0300

    Update cadastrarComputador.php

--- cadastrarComputador.php
+++ cadastrarComputador.php
@@ -9,6 +9,6 @@ extract($_POST);
 
 $chkAtualizado = isset($_POST['chkAtualizado']);
 
-$novoCadastro = new Computador($txtProcessador, $txtUSB,
-        $chkAtualizado, '2022-02-24 09:30:00');
+$novoCadastro = new Computador($txtPlaca,
+        '2022-02-24 09:30:00');
 $novoCadastro->Cadastrar();
