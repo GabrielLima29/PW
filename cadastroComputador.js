ccabee6249a71d9d30f5bcb4fc25801d095d459f
GabrielLima29 <101425770+GabrielLima29@users.noreply.github.com>
2022-04-07 23:51:18 -0300

    Update cadastroComputador.js

--- cadastroComputador.js
+++ cadastroComputador.js
@@ -12,12 +12,29 @@ function enviar(event){
 }
 
 function cadastrar(){
-    let formData = new FormData(formulario);
-    fetch("http://3tdsn2022.atwebpages.com/cadastrarComputador.php",
-    {
-        method:'POST',
-        body:formData
-    }).then(resposta => resposta.text())
-      .then(texto => console.log(texto));
-    return false;
+    let txtProcessador = document.querySelector("#txtProcessador");
+    let txtUSB = document.querySelector("#txtUSB");
+    let chkAtualizado = document.querySelector("#chkAtualizado");
+    let txtDataAtualizacao = document.querySelector("#txtDataAtualizacao");
+
+    form.addEventListener("submit" , function(event){
+        event.preventDefault();
+
+        let dados = {
+            txtProcessador: txtProcessador.value,
+            txtUSB: txtUSB.value,
+            chkAtualizado: chkAtualizado.value,
+            txtDataAtualizacao: txtDataAtualizacao.value,
+
+        }
+
+        fetch("http://3tdsn2022.atwebpages.com/cadastrarComputador.php", {
+         method: POST,
+         body: JSON.stringify(dados),
+         
+        })
+         .then(function(responde) {
+            alert("Cadastrado(a) com Sucesso!")
+        })
+    }
 }
